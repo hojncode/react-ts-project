f8e02bf4486071e9e99cbd3a3223c04f41bb7807
hojncode <glicm12@gmail.com>
2022-12-07 18:13:39 +0900

    interface

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,21 +1,18 @@
 import styled from "styled-components";
 
-interface ContainerProps{
+// const x = (a:number, b:number) => a + b 
+// 이렇게 타입을 설정하는 방법 대신, 아래의 interface로 사용할 수 있다.
+interface CircleProps {
     bgColor:string;
 }
 
-const Container = styled.div<ContainerProps>`
+// Container 에도 따로 ContainerProps를 만들어줘야 하지만, CircleProps와 내용이 같기 때문에 여기서는 하나로 사용하였음.
+const Container = styled.div<CircleProps>`
     width: 200px;
     height: 200px;
     background-color: ${props => props.bgColor};
     border-radius: 50%;
 `;
-
-// const x = (a:number, b:number) => a + b
-
-interface CircleProps {
-    bgColor:string;
-}
 
 function Circle({bgColor}: CircleProps) {
     return <Container bgColor={bgColor}/>;
@@ -23,14 +20,19 @@ function Circle({bgColor}: CircleProps) {
 
 export default Circle;
 
-interface PlayerShape {
-    name:string;
-    age: number;
-    hello: number;
-}
-
-const sayHello = (plyerObj:PlayerShape) => `Hello ${plyerObj.name} you are ${plyerObj.age}`;
 
 
-sayHello({name:"hojn", age: 12, hello:2})
-sayHello({name:"hi", age:12, hello:1})
+
+
+// ===========================================================//
+// ** interface 사용 예시 
+// interface PlayerShape {
+//     name:string;
+//     age: number;
+//     hello: number;
+// }
+
+// const sayHello = (plyerObj:PlayerShape) => `Hello ${plyerObj.name} you are ${plyerObj.age}`;
+
+// sayHello({name:"hojn", age: 12, hello:2})
+// sayHello({name:"hi", age:12, hello:1})
