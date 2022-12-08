6ed36c22389b8f06d6ea9f0a10039726014b9e1a
hojncode <glicm12@gmail.com>
2022-12-08 23:32:11 +0900

    Route States - useEffect->fetch() , array.map , <Link> , ternary operator, useLocation

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,13 +1,110 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+    padding:0px 20px;
+`
+
+const Header = styled.header`
+    height: 10vh;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+
+const CoinsList = styled.ul`
+    
+`
+const Coin = styled.div`
+    background-color:white;
+    color:${props => props.theme.bgColor};
+    margin-bottom: 10px;
+    border-radius: 15px;
+    a {
+        display:flex;
+        align-items:center;
+        transition: color 0.2s ease-in;
+        // div 전체를 클릭해도 되게 만듬.
+        padding: 20px;
+    }
+    &:hover {
+        a {
+            color: ${props => props.theme.accentColor}
+        }
+    }
+
+`
 
 const Title = styled.h1`
+    font-size: 48px;
     color:${props => props.theme.accentColor};
 `;
 
+const Img = styled.img`
+    width:20px;
+    height:20px;
+    margin-right: 10px;
+`
+
+const Loader = styled.span`
+    text-align: center;
+    display:block;
+    font-size:48px;
+`
+
+interface CoinInterface {
+        id: string,
+        name: string,
+        symbol: string,
+        rank: number,
+        is_new: boolean,
+        is_active:boolean,
+        type: string,
+}
+
+
 
 function Coins() {
-    return <Title>코인</Title>
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(()=> {
+        (async() => {
+            const response = await fetch("https://api.coinpaprika.com/v1/coins");
+            const json = await response.json();
+            setCoins(json.slice(0,100));
+            // 내가 추가한 코드 setTimeout
+            setTimeout(() => {setLoading(false);}, 1000);
+        })();
+    },[]);
+    return (
+    <Container>
+        <Header>
+            <Title>코인</Title>
+        </Header>
+        {loading ? <Loader>"Loading..."</Loader> : (
+                <CoinsList>
+                    {coins.map((coin) => (
+                    <Coin key={coin.id}>
+                        <Link to={{
+                            pathname: `/${coin.id}`,
+                            state:{name: coin.name},
+                        }}>
+                            <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}/>
+                            {coin.name} &rarr;
+                        </Link>
+                        {/* <a href={`/${coin.id}`}>
+                            <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}/>
+                            {coin.name} &rarr;
+                        </a> */}
+                    </Coin>
+                    ))}
+                </CoinsList>
+        )}
+    </Container>
+    
+    
+    )
 }
 
 export default Coins;
--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme:DefaultTheme = {
     bgColor: "#2f3640",
-    textColor:"#f5f6fa",
+    textColor:"#8c7ae6",
     accentColor:"#4cd137"
 
 }
