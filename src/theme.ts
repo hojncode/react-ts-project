038aa4f8341d18cee5f8add28b0debdcb9a28b28
hojncode <glicm12@gmail.com>
2022-12-10 03:09:17 +0900

    Theme mode

--- src/theme.ts
+++ src/theme.ts
@@ -1,21 +1,19 @@
-import { DefaultTheme } from 'styled-components';
+import { DefaultTheme } from "styled-components";
 
-export const theme:DefaultTheme = {
-    bgColor: "#2f3640",
-    textColor:"#8c7ae6",
-    accentColor:"#4cd137"
+// export const theme: DefaultTheme = {
+//   bgColor: "#2f3640",
+//   textColor: "#8c7ae6",
+//   accentColor: "#4cd137",
+// };
 
-}
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  accentColor: "tomato",
+};
 
-// export const lightTheme:DefaultTheme = {
-//     bgColor: "white",
-//     textColor:"black",
-//     btnColor:"tomato"
-    
-// }
-
-// export const darkTheme:DefaultTheme = {
-//     bgColor: "black",
-//     textColor:"white",
-//     btnColor:"teal"
-// }
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  accentColor: "teal",
+};
