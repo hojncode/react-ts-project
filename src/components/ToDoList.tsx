bc241aeb40f8329b92955a8465b9f4d4f9cbb754
hojncode <glicm12@gmail.com>
2022-12-14 19:43:10 +0900

    Immutability2 - Array input properties

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -9,7 +9,7 @@ function ToDoList() {
   // const modFn = useSetRecoilState(toDoState);
   // 위 두줄을 아래 한줄로 합쳐서 사용할 수 있다.
   const toDos = useRecoilValue(toDoState);
-  // console.log(toDos);
+  console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
@@ -26,3 +26,16 @@ function ToDoList() {
 }
 
 export default ToDoList;
+
+// // * 배열에 요소 교체하기 , (배열의 순서는 고정되어있다 - 불변)
+// const food = ["pizza", "mango", "kimchi", "kimbab"]
+// const front = ["pizza"]
+// const back = ["kimchi", "kimbab"]
+// const finalPart = [...front,"감", ...back]
+
+//  // slice 인자에 하나만 넣으면 그 인자의 숫자부터 마지막까지 범위가 지정된다.
+// const food = ["pizza", "mango", "kimchi", "kimbab"]
+// const target = 1;
+// food.slice(0,1) -> ["pizza"]
+// food.slice(target+1) -> ["kinchi", "kimbab"]
+// [...food.slice(0, target), "감", ...food.slice(target+1)] -> ["pizza" , "감", "kinchi", "kimbab"]
